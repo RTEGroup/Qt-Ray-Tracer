8741c741b0bca15e8d84e779bd2d4b687277207f
Sruthik P <psruthik@gmail.com>
2017-02-10 20:46:12 +0000

    Added Updated Vector Class

--- sVector.h
+++ sVector.h
@@ -1,3 +1,17 @@
+/*
+Primer on Vectors:
+
+Normalize: Maintaining the same directon of the Vector but reducing it to unit length
+           Done by dividing the Vector with its length
+
+Dot Product : Duh. Algebraically multiplying the components
+              Geometrically the projection of one Vector on another 
+
+Cross Product : Geometrically the area formed by a parallelogram
+
+*/
+
+
 #ifndef SVECTOR_H
 #define SVECTOR_H
 
@@ -14,13 +28,13 @@ namespace sVec
   
  	 	T x,y,z;
   
-  		Vec3(): x(T(0)), y(T(0)), z(T(0)) {}
+  		inline Vec3(): x(T(0)), y(T(0)), z(T(0)) {}
   	
-  		Vec3(T val): x(val), y(val), z(val) {}
+  		inline Vec3(T val): x(val), y(val), z(val) {}
 
-  		Vec3(T x_val, T y_val, T z_val): x(x_val), y(y_val), z(z_val) {}
+  		inline Vec3(T x_val, T y_val, T z_val): x(x_val), y(y_val), z(z_val) {}
 
-  		Vec3& normalize()
+  		inline Vec3& normalize()
   		{	
 
   			T nor_sq = length_sq();
@@ -32,63 +46,90 @@ namespace sVec
   			return *this;
   		}
 
- 		Vec3<T> operator * (const T &f) const
+ 		  inline Vec3<T> operator * (const T &f) const
   		{
   			return Vec3<T>(x*f, y*f, z*f);
   		}
 
-  		Vec3<T> operator * (const Vec3<T> &v) const
+  		inline Vec3<T> operator * (const Vec3<T> &v) const
   		{
   			return Vec3<T>(x*v.x, y*v.y, z*v.z);
   		}
 
-  		Vec3<T> operator + (const Vec3<T> &v) const
+      /*
+      inline Vec3<T> operator * (const T &f, const Vec3<T> &v) 
+      {
+        return Vec3<T>(v.x*t, v.y*t, v.z*t);
+      }
+      */
+
+  		inline Vec3<T> operator + (const Vec3<T> &v) const
   		{
   			return Vec3<T>(x+v.x, y+v.y, z+v.z);
- 		}
+  		}
 
-  		Vec3<T> operator - (const Vec3<T> &v) const
+  		inline Vec3<T> operator - (const Vec3<T> &v) const
   		{
   			return Vec3<T>(x-v.x, y-v.y, z-v.z);
   		}
 
-  		Vec3<T> operator -() const
- 		{
+  		inline Vec3<T> operator -() const
+ 		  {
   			return Vec3<T>(-x,-y,-z);
- 		}
-
-  		Vec3<T> operator += (const Vec3<T> &v) 
- 		{
+ 		  }
+  
+  		inline Vec3<T> operator += (const Vec3<T> &v) 
+ 		  {
   			Vec3<T>(x+=v.x, y+=v.y, z+=v.z);
   			return *this;
- 		}
+ 		  }
   
- 		Vec3<T> operator *= (const Vec3<T> &v)
+ 		  inline Vec3<T> operator *= (const Vec3<T> &v)
   		{
   			Vec3<T>(x*=v.x, y*=v.y, z*=v.z);
   			return *this;
- 		}
+ 		  }
 
- 		T dot(const Vec3<T> &v) const 
- 		{ 
- 			return x * v.x + y * v.y + z * v.z; 
- 		}
+      inline Vec3<T> operator / (const Vec3<T> &v) const
+      {
+        return Vec3<T>(x/v.x, y/v.y, z/v.z);
+      }
 
- 		T length_sq() const
-		{
-			return x*x+y*y+z*z;
-		}
+      inline Vec3<T> operator / (const T &f) const
+      {
+        return Vec3<T>(x/f, y/f, z/f);
+      }
 
- 		T length() const
-		{
-			return sqrt(length_sq());
-		}
+ 		  inline T dot(const Vec3<T> &v) const 
+ 		  { 
+ 			  return x * v.x + y * v.y + z * v.z; 
+ 		  } 
 
-  		friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
- 		{
-			os<< "["<<v.x<<" "<<v.y<<" "<<v.z<<"]";
-			return os;
- 		}
+      inline T cross(const Vec3<T> &v)const
+      {
+        return Vec3<T>((y*v.z-z*v.y), (-(x*v.z-z*v.x)), (x*v.y-y*v.x));
+      }
+
+ 		  inline T length_sq() const
+		  {
+			 return x*x+y*y+z*z;
+		  }
+
+ 		  inline T length() const
+		  {
+			 return sqrt(length_sq());
+		  }
+
+  		inline friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
+ 		  {
+			 os<< "["<<v.x<<" "<<v.y<<" "<<v.z<<"]";
+			 return os;
+ 		  }
+
+      inline friend std::istream & operator >> (std::istream &is, const Vec3<T> &v)
+      {
+        is>>v.x>>v.y>>v.z;
+      }
 	};
 
 typedef Vec3<float> Vec3f;
